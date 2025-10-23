60ff26c1ef8f568292bbd1672fc73346f851dd24
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-23 17:00:58 -0400

    provided code compiling and missing stuff filled

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -5,12 +5,12 @@ public class Application {
     public static void main(String[] args) {
         Deck deck = new Deck();
         Hand hand1 = new Hand();
-// deal 5 cards
-        for(int I = 0; I < 5; i++) {
-// get a card from the deck
+        // deal 5 cards
+        for(int i = 0; i < 5; i++) {
+            // get a card from the deck
             Card card = deck.deal();
-// deal that card to the hand
-            hand1.deal(card)
+            // deal that card to the hand
+            hand1.deal(card);
         }
         int handValue = hand1.getValue();
         System.out.println("This hand is worth " + handValue);
